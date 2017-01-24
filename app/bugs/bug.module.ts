63242f1108d987966d46b9d0a043220f55082df4
manolis9 <manolis@alumni.ubc.ca>
2017-01-24 13:36:56 -0800

    Created a bug-list component

--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -1,11 +1,17 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+
+// Component
+import { BugListComponent } from './bug-list/bug-list.component';
 
 @NgModule ({
     imports: [
         SharedModule
      ],
-    declarations: [ ],
+    declarations: [
+        BugListComponent
+     ],
     exports: [ ],
     providers: [ ]
 })
