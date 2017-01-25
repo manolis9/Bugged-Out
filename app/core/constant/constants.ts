0c96599d04aef360e98c09566d6824ab84ecff12
manolis9 <manolis@alumni.ubc.ca>
2017-01-25 10:01:28 -0800

    Set up with Firebase

---
+++ app/core/constant/constants.ts
@@ -0,0 +1,7 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBNkHXwpLxOpFG01KCAij6upKjaDEEbsks",
+    authDomain: "bugged-out-6f8c6.firebaseapp.com",
+    databaseURL: "https://bugged-out-6f8c6.firebaseio.com",
+    storageBucket: "bugged-out-6f8c6.appspot.com",
+    messagingSenderId: "149858173565"
+};
